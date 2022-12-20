e82b6ebee186d53dafe96f448aaa58f30ffe7c6d
RUzai <RUzairov@gmail.com>
2022-12-20 21:58:32 +0300

    test init

--- src/test/java/ru/sfedu/goslingdrive/api/ApiTest.java
+++ src/test/java/ru/sfedu/goslingdrive/api/ApiTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.sfedu.goslingdrive.utils.TestData;
 
-
 public class ApiTest extends TestData {
     AbstractDataProvider dp = new DataProviderXml();
 
@@ -15,6 +14,17 @@ public class ApiTest extends TestData {
 
     @BeforeEach
     void setUp() {
+        dp.insertBodyPart(b1);
+        dp.insertBodyPart(b2);
+
+        dp.insertElectricPart(e1);
+        dp.insertElectricPart(e2);
+
+        dp.insertRunningPart(r1);
+        dp.insertRunningPart(r2);
+
+        dp.insertUser(u1);
+        dp.insertUser(u2);
 
         dp.insertOrder(o1);
         dp.insertOrder(o2);
@@ -22,6 +32,17 @@ public class ApiTest extends TestData {
 
     @AfterEach
     void tearDown() {
+        dp.deleteBodyPart(b1.getId());
+        dp.deleteBodyPart(b2.getId());
+
+        dp.deleteElectricPart(e1.getId());
+        dp.deleteElectricPart(e2.getId());
+
+        dp.deleteRunningPart(r1.getId());
+        dp.deleteRunningPart(r2.getId());
+
+        dp.deleteUser(u1.getId());
+        dp.deleteUser(u2.getId());
 
         dp.deleteOrder(o1.getId());
         dp.deleteOrder(o2.getId());
